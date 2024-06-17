16426cbe62de24e42f825d36fe372d2c3624de9b
Matin De Wet <matindewet116@gmail.com>
2024-06-17 08:23:38 +0200

    Add base models for Entity generation

---
+++ DomainBaseEntity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace DomainBaseEntity
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        public DateTime? DateDeleted { get; set; }
+
+        public DateTime? DateUpdated { get; set; }
+    }
+}
